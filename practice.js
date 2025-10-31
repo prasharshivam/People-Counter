d0e53a1dd9ea299b02c37a4765a5acdf6dc0debe
Shivam Bharti <42884506+prasharshivam@users.noreply.github.com>
2025-10-31 06:50:34 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,3 +1,4 @@
+let saveEl = document.getElementById("save-El")
 let countEl = document.getElementById("count-El")
 console.log(countEl)
 
@@ -6,4 +7,12 @@ let count = 0
 function increment() {
     count = count + 1
     countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + "-"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+
 }
